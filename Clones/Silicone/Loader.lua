704bdaba2f5095bf77beb6628845b8e74912462c
Ache <ghgghigher@gmail.com>
2026-02-17 22:58:39 -0500

    Si

---
+++ Clones/Silicone/Loader.lua
@@ -0,0 +1,18 @@
+print("CRACKED BY SIGMA X CORD X ACHE")
+local Games = {
+    [286090429] = "",
+    [7711635737] = "",
+    [891852901] = "",
+    [136801880565837] = "",
+    [94539932414164] = "",
+    [142823291] = "",
+    [6161235818] = "",
+    [5104202731] = "",
+    [89570211388424] = ""
+}
+
+local LoaderUrl = Games[game.PlaceId]
+if LoaderUrl then
+    loadstring(game:HttpGet(LoaderUrl))()
+end
+print("CRACKED BY SIGMA X CORD X ACHE")
